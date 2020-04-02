b304c3c521db93723ed61f23315ecb6143855df1
William Assis <Willifran@gmail.com>
2020-04-01 21:04:49 -0300

    Criou o FornecdorBean e metado salvar realizando atraves do formulario fornecedorcadastro

--- src/main/java/br/com/vendaonline1/bean/FornecedorBean.java
+++ src/main/java/br/com/vendaonline1/bean/FornecedorBean.java
@@ -5,6 +5,9 @@
  */
 package br.com.vendaonline1.bean;
 
+import br.com.vendaonline1.dao.FornecedorDao;
+import br.com.vendaonline1.domain.Fornecedor;
+import br.com.vendasonline1.util.JSFUtil;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
@@ -15,5 +18,37 @@ import javax.faces.bean.ViewScoped;
 @ManagedBean(name = "MBFornecedor")
 @ViewScoped
 public class FornecedorBean {
+    
+    private Fornecedor fornecedor;
+    
+    public void salvar(){
+        
+        try{
+            FornecedorDao fdao = new FornecedorDao();
+             fdao.salvar(fornecedor);
+             
+        
+        JSFUtil.adicionarMensagemSucesso("Salvo com sucesso");
+        
+        
+             fornecedor = new Fornecedor();
+        
+        } catch(RuntimeException e){
+            JSFUtil.adicionarMensagemErro("ex.getMessage()");
+            e.printStackTrace();
+        }
+    }
+
+    public Fornecedor getFornecedor() {
+        if(fornecedor == null){
+            fornecedor = new Fornecedor();
+        }
+        return fornecedor;
+       
+    }
+
+    public void setFornecedor(Fornecedor fornecedor) {
+        this.fornecedor = fornecedor;
+    }
     
 }
